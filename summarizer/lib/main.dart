c35e2cd17263cbf3204df44710250de178367582
Aftab-M <93897204+Aftab-M@users.noreply.github.com>
2024-01-23 22:10:06 +0530

    Integrations

--- summarizer/lib/main.dart
+++ summarizer/lib/main.dart
@@ -1,3 +1,8 @@
+import 'dart:html';
+import 'dart:typed_data';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:file_picker/src/platform_file.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +21,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Summarizer'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -36,18 +42,33 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              "",
-              style: Theme.of(context).textTheme.headlineMedium,
+            const Text('Provide a PDF'),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () async {
+                // var file =
+                // await FilePicker.platform.pickFiles(type: FileType.any);
+                // Uint8List uploadedFile;
+                FilePickerResult? result =
+                    await FilePicker.platform.pickFiles();
+                if (result != null) {
+                  Uint8List? fileBytes = result.files.first.bytes;
+                  String fileName = result.files.first.name;
+                  debugPrint(" File name : ${fileName} ");
+                }
+              },
+              child: const Text('Pick a file'),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.all(20)),
             ),
           ],
         ),
